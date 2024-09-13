566642003efa001706bb2fbbcaa27f6cad433a1d
Kartik Gill <Kartik.Gill@deephealth.com>
2024-09-13 17:34:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto"> {/*sx make sure this device is responsive on larger devices*/}
            <Navbar />
       <Routes>
-        <Route path = "/" element = {<Home />} /> {/*to setup home page using route*/}     
+        <Route path = "/Fitness_App/" element = {<Home />} /> {/*to setup home page using route*/}     
         <Route path = "/exercise/:id" element = {<ExerciseDetail />} />  {/*here :id means we can dynamically id like 1,2,3 etc*/}
       </Routes>
       <Footer />
